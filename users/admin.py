b139707898fec0631a2896acfe5bdc474b8cbc0f
AzizM07 <mohamed.aziz.mehri.pro@gmail.com>
2026-07-13 02:39:28 +0100

    Register all models in Django admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from .models import User
 
-# Register your models here.
+# Enregistre tous les modèles de l'app users automatiquement
+from django.apps import apps
+for model in apps.get_app_config('users').get_models():
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
